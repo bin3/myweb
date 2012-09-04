be25ff10f59d08e226558d83db2f96210421fbc3
Binson Zhang <bin183cs@gmail.com>
2012-09-04 22:22:19 +0800

    Add compile<<, >>, match<<, >>.

--- js/ac-d3.js
+++ js/ac-d3.js
@@ -24,9 +24,15 @@ balgo.ac = {
 	Matcher: function() {
 		var id = 0;
 		var root = new balgo.ac.Node(id++, 'root');
+		// for animation
+		var bfs_nodes = [];
+		var bfs_idx = -1;
+		var text_ = '';
+		var match_trace_ = [];
+		var match_idx_ = -1;
 		
 		this.insert = function (key) {
-			console.log('insert key: ' + key);
+//			console.log('insert key: ' + key);
 			var cur = root;
 			for (var i = 0; i < key.length; ++i) {
 				c = key[i];
@@ -41,6 +47,9 @@ balgo.ac = {
 		
 		this.compile = function () {
 			console.log('compile');
+			bfs_nodes = [];
+			bfs_idx = -1;
+			
 			root.fail = root;
 			var q = [root];
 			while (q.length > 0) {
@@ -48,6 +57,7 @@ balgo.ac = {
 				for (var cl in p.children) {
 					var child = p.children[cl];
 					q.push(child);
+					bfs_nodes.push(child);
 					// find fail
 					var cf = p.fail;
 					if (p != root) {
@@ -71,15 +81,56 @@ balgo.ac = {
 				}
 			}
 		};
+		this.forwardCompile = function () {
+			++bfs_idx;
+			if (bfs_idx >= bfs_nodes.length) {
+				bfs_idx = 0;
+				$('path.fail').hide();
+				$('path.report').hide();
+				return;
+			}
+			for (; bfs_idx < bfs_nodes.length; ++bfs_idx) {
+				var node = bfs_nodes[bfs_idx];
+				if (node.fail != root) {
+//					var circle = $('#' + nodeId(node) + ' circle').addClass('focus');
+//					console.log(circle);
+					$('#' + edgeId(node, node.fail, 'fail')).show('slow');
+					if (node.report) {
+						$('#' + edgeId(node, node.report, 'report')).show('slow');
+					}
+					break;
+				}
+			}
+		};
+		this.backCompile = function () {
+			for (; bfs_idx >= 0; --bfs_idx) {
+				var node = bfs_nodes[bfs_idx];
+				if (node.fail != root) {
+					$('#' + edgeId(node, node.fail, 'fail')).hide();
+					if (node.report) {
+						$('#' + edgeId(node, node.report, 'report')).hide();
+					}
+					--bfs_idx;
+					break;
+				}
+			}
+		};
 		
 		this.match = function (text) {
 			console.log('match key:' + text);
+			text_ = text;
+			match_trace_ = [];
+			match_idx_ = -1;
+			
 			var keys = [];
 			var cur = root;
+//			match_trace_.push({'tidx': 0, 'node': cur});
 			for (var i = 0; i < text.length; ++i) {
 				c = text[i];
+				match_trace_.push({'tidx': i, 'node': cur});
 				while (!(c in cur.children) && cur != root) {
 					cur = cur.fail;
+					match_trace_.push({'tidx': i, 'node': cur});
 				}
 				if (c in cur.children) {
 					cur = cur.children[c];
@@ -92,9 +143,56 @@ balgo.ac = {
 						rp = rp.report;
 					}
 				}
+//				match_trace_.push({'tidx': i, 'node': cur});
 			}
+			match_trace_.push({'tidx': text.length, 'node': cur});
+			console.log(match_trace_);
 			return keys;
 		};
+		function addClass(selector, cls) {
+			var new_class = $(selector).attr('class') + ' ' + cls;
+			$(selector).attr('class', new_class);
+		}
+		function removeClass(selector, cls) {
+			var new_class = $(selector).attr('class').replace(cls, '');
+			$(selector).attr('class', new_class);
+		}
+		function updateMatchInfo(match_idx) {
+			console.log(match_idx);
+			if (match_idx < 0 || match_idx >= match_trace_.length) return;
+			var trace = match_trace_[match_idx];
+			var ch = (trace.tidx < text_.length) ? text_[trace.tidx] : '';
+			$('#match-info').text('[' + trace.tidx + '] ' + ch);
+			var selector = '#' + nodeId(trace.node) + ' circle';
+			addClass(selector, 'focus');
+		}
+		function clearMatchInfo(match_idx) {
+			if (match_idx < 0 || match_idx >= match_trace_.length) return;
+			var trace = match_trace_[match_idx];
+			$('#match-info').text('');
+			var selector = '#' + nodeId(trace.node) + ' circle';
+			removeClass(selector, 'focus');
+		}
+		this.forwardMatch = function() {
+			clearMatchInfo(match_idx_);
+			if ($('#text').val() != text_) {
+				this.match($('#text').val());
+			}
+			++match_idx_;
+			if (match_idx_ >= match_trace_.length) {
+				match_idx_ = -1;
+				return;
+			}
+			updateMatchInfo(match_idx_);
+		};
+		this.backMatch = function() {
+			clearMatchInfo(match_idx_);
+			if (match_idx_ < 0) return;
+			--match_idx_;
+			updateMatchInfo(match_idx_);
+		};
+		
+		
 		this.clear = function() {
 			function dfs(p) {
 				for (var cl in p.children) {
@@ -106,13 +204,22 @@ balgo.ac = {
 			root = null;
 			id = 0;
 			root = new balgo.ac.Node(id++, 'root');
+			bfs_nodes = [];
+			bfs_idx = -1;
 		};
 		
 		function nodeId(node) {
+			return 'node' + node.id;
+		}
+		function nodeName(node) {
 			return node.id + '-' + node.label;
 		}
+		function edgeId(src, tgt, type) {
+			return type + src.id + '-' + tgt.id;
+		}
 		function edge(src, tgt, type) {
-			return {'source': nodeId(src), 'target': nodeId(tgt), 'type': type};
+			return {'source': nodeName(src), 'target': nodeName(tgt),
+				'type': type, 'id': edgeId(src, tgt, type)};
 		}
 		this.treeData = function() {
 			function dfs(node, edges) {
@@ -122,7 +229,7 @@ balgo.ac = {
 				if (node.report) {
 					edges.push(edge(node, node.report, 'report'));
 				}
-				var json = {'name': nodeId(node), 'children': []};
+				var json = {'id': nodeId(node), 'name': nodeName(node), 'final': node.final, 'children': []};
 				for (var label in node.children) {
 					child = node.children[label];
 					edges.push(edge(node, child, 'normal'));
@@ -162,8 +269,9 @@ function drawTrie(svg, json, edges) {
 	var id2node = {};
 	nodes.forEach(function(node) { id2node[node.name] = node; });
 	
-	var links = gtree.selectAll("path.link").data(edges)
+	gtree.selectAll("path.link").data(edges)
 		.enter().append("path")
+		.attr("id", function(d) { return d.id; })
 		.attr("class", function(d) { return "link " + d.type;})
 	    .attr("marker-end", function(d) { return "url(#" + d.type + ")"; })
 	    .attr("d", function(d) {
@@ -182,17 +290,26 @@ function drawTrie(svg, json, edges) {
 	    	}
 	    	return "M" + src.x + "," + src.y + " L" + tgt.x + "," + tgt.y;
 	  });
-	gtree.selectAll('path.fail').attr('visibility', 'hidden');
-	gtree.selectAll('path.report').attr('visibility', 'hidden');
-//	console.log(gtree.selectAll('path.report'));
+	
+	// hide fail and report pointers at first
+	$('path.fail').hide();
+	$('path.report').hide();
 	
 	var node = gtree.selectAll("g.node")
 		.data(nodes, function(d) { return d.name; })
 		.enter().append("g")
-		.attr("class", "node")
+		.attr("id", function(d) { return d.id; })
+		.attr("class", 'node')
 		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-	node.append("circle").attr("r", radius);
+	node.append("circle").attr("r", radius)
+	.attr("class", function(d) {
+		if (d.final) {
+			return 'final';
+		} else {
+			return '';
+		}
+	});
 
 	node.append("text").attr("dx", 0)
 		.attr("dy", 4)
@@ -241,7 +358,6 @@ function initSvg() {
 
 function insert() {
 	var text = $('#keys').val();
-	//console.log(text);
 	var keys = text.split('\n');
 	keys.forEach(matcher.insert);
 }
@@ -250,11 +366,11 @@ $(document).ready(function(){
 	console.log('AC demo started');
 
 	var svg = initSvg();
-	
-	$('#keys').val('abcd\nbc\n码农\n码农abc\n农\n我是码农');
-	$('#text').val('码农啊abcd');
+
 //	$('#keys').val('abc\nbc\nb');
 //	$('#text').val('abcd');
+	$('#keys').val('南京\n南京市\n南京市长\n市长\n江大桥\n长江\n大桥\n长江大桥');
+	$('#text').val('南京市长江大桥');
 	
 	//ac = new balgo.ac();
 	//matcher = new ac.Matcher();
@@ -281,17 +397,31 @@ $(document).ready(function(){
 		var data = matcher.treeData();
 		drawTrie(svg, data.json, data.edges);
 	});
-	$('#genfail').click(function() {
-		toggle(svg, 'path.fail');
+	$('#toggle-fail').click(function() {
+		$('path.fail').toggle();
 	});
-	$('#genreport').click(function() {
-		toggle(svg, 'path.report');
+	$('#toggle-report').click(function() {
+		$('path.report').toggle();
 	});
 
 	$('#genall').click(function() {
 		$('#gentrie').click();
-		$('#genfail').click();
-		$('#genreport').click();
+		$('path.fail').show();
+		$('path.report').show();
+	});
+	
+
+	$('#forwardCompile').click(function() {
+		matcher.forwardCompile();
+	});
+	$('#backCompile').click(function() {
+		matcher.backCompile();
+	});
+	$('#forwardMatch').click(function() {
+		matcher.forwardMatch();
+	});
+	$('#backMatch').click(function() {
+		matcher.backMatch();
 	});
 	
 	$('#clear').click(function() {
